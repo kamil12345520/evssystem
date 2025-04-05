ffd70e04baed42fc49b2720be89e1bf1675d19a3
kamil12345520 <kamilmehtab30@gmail.com>
2025-04-06 02:59:12 +0500

    add other work done today

--- php/action/register.php
+++ php/action/register.php
@@ -1,4 +1,15 @@
-<?php 
-$name = $_POST['name'];
-echo $name;
-?>
+<?php
+  $name= $_POST['name'];
+  $email= $_POST['email'];
+  $password= $_POST['password'];
+  $cpassword= $_POST['confirm_password'];
+  $dob= $_POST['dob'];
+  $gender= $_POST['gender'];
+  //condition
+  if ($password == $cpassword){
+    echo 'password is matched';
+  }else{
+    //redirect
+    header('Location:.../register.php');
+  }
+?> 
