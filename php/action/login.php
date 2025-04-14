a8b495376bffb14f2a6c379a9d8e4523d387e396
kamil12345520 <kamilmehtab30@gmail.com>
2025-04-15 00:18:29 +0500

    adding the today work

--- php/action/login.php
+++ php/action/login.php
@@ -6,7 +6,12 @@ $query = "SELECT * FROM users WHERE email= '$email'  AND '$password'";
 $result = mysqli_query($cn,$query) or die('cant run query');
 $rows= mysqli_num_rows($result);
  if ($rows >0){
+   //firstly website find the data after they start session session say that yes the data
+   //in the database and then any site they are showing the required data that we need.
+   $row = mysqli_fetch_array($result);// database mne sy data ko array ke form mne fetch krta hy
+   session_start();
     header ('Location:../home.php');
+    $_SESSION['users']= $row['name'];
 
  }else {
     $warning = "email and password did not correct";
--- php/home.php
+++ php/home.php
@@ -1,7 +1,12 @@
-<?php 
-include('./include/header.php');
+ 
+<?php include('./include/header.php');
+session_start();
+if(empty ($_SESSION ['users'])){
+    header('Location:index.php');
+}
 ?>
-<h1> Home Page </h1>
-<?php 
-include('./include/footer.php');
-?>
+<div class= "container">
+<h1> welcome <?php echo $_SESSION['users'];?> </h1>
+</div> 
+<?php include('./include/footer.php');?>
+
